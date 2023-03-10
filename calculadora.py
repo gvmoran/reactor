b4656f8953522666d3f5461129d3d32f48acf5f1
Gerardo Moran Cruz <gerardo.moran@ndeveloper.com>
2023-03-10 17:39:25 -0500

    tercer commit

--- calculadora.py
+++ calculadora.py
@@ -5,6 +5,12 @@ def suma(numero1, numero2):
     total = numero1 + numero2
     return total
 
-resultado = suma(num1, num2)
-print('El resultado es: ',resultado)
+def resta(numero1, numero2):
+    total = numero1 - numero2
+    return total
 
+resultado = suma(num1, num2)
+print('El resultado de la suma es: ',resultado)
+
+resultado = resta(num1, num2)
+print('El resultado de la resta es: ',resultado)
